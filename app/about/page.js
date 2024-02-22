c2e8d5d0d6c1379d023a32c2cf17358cf5e4ea2c
Theaceemperor <omoobajesuexcellent@gmail.com>
2024-02-22 13:05:16 +0100

    about page resolution

--- app/about/page.js
+++ app/about/page.js
@@ -1,7 +1,4 @@
-'use client'
 
-import React from "react";
-import { useIsVisible } from "@/components/useIsVisible";
 import Image from "next/image";
 
 // Dummy data for the About Page
@@ -28,20 +25,18 @@ const aboutData = {
 };
   
 export default function AboutPage() {
-    const ref = React.useRef();
-    const isVisible1 = useIsVisible(ref);
 
     return (
         <main className="max-w-7xl mx-auto px-4 py-8">
             <section className="mb-8">
                 <h1 className="text-3xl font-bold text-gray-800 dark:text-white mb-4">Personal Bio</h1>
-                <p className={`text-gray-600 dark:text-gray-400`}>{aboutData.bio}</p>
+                <p className={"text-gray-600 dark:text-gray-400"}>{aboutData.bio}</p>
             </section>
             <section className={"mb-8"}>
                 <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">Skills and Expertise</h2>
                 <ul className={`list-disc list-inside `}>
                     {aboutData.skills.map((skill, index) => (
-                        <li key={index} className={`text-gray-600 dark:text-gray-400`}>{skill}</li>
+                        <li key={index} className={"text-gray-600 dark:text-gray-400"}>{skill}</li>
                     ))}
                 </ul>
             </section>
@@ -49,13 +44,13 @@ export default function AboutPage() {
                 <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">Awards and Recognition</h2>
                 <ul className={"list-disc list-inside"}>
                     {aboutData.awards.map((award, index) => (
-                        <li key={index} className={`text-gray-600 dark:text-gray-400`}>{award}</li>
+                        <li key={index} className={"text-gray-600 dark:text-gray-400"}>{award}</li>
                     ))}
                 </ul>
             </section>
             <section>
                 <h2 className="text-2xl font-bold text-gray-800 dark:text-white mb-4">Photo Gallery</h2>
-                <div className={`grid grid-cols-2 md:grid-cols-4 gap-4`}>
+                <div className={"grid grid-cols-2 md:grid-cols-4 gap-4"}>
                     {aboutData.photoGallery.map((photo, index) => (
                         <Image width={500} height={500} key={index} src={photo} alt={`Photo ${index + 1}`} className={"w-full h-auto rounded-md shadow-md"} />
                     ))}
